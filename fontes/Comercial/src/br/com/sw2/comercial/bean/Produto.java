7096e51625798f0eb304f9274aa33e8e9682ac62
Marcelo Santos <marcelo68ms@gmail.com>
2014-04-27 19:00:41 -0300

    Desenvolvimento do Código

--- fontes/Comercial/src/br/com/sw2/comercial/bean/Produto.java
+++ fontes/Comercial/src/br/com/sw2/comercial/bean/Produto.java
@@ -8,46 +8,44 @@ package br.com.sw2.comercial.bean;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 /**
  *
  * @author marcelo
  */
 @Entity
-@Table(name="TblFornecedor")
-public class Fornecedor implements Serializable {
+public class Produto implements Serializable {
     private static final long serialVersionUID = 1L;
-    
     @Id
-    private String cnpj;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    private String razaoSocial;
-    private String endereco;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String cep;
-    private String email;
-    private String fone1;
-    private String fone2;
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (cnpj != null ? cnpj.hashCode() : 0);
+        hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Fornecedor)) {
+        if (!(object instanceof Produto)) {
             return false;
         }
-        Fornecedor other = (Fornecedor) object;
-        if ((this.cnpj == null && other.cnpj != null) || (this.cnpj != null && !this.cnpj.equals(other.cnpj))) {
+        Produto other = (Produto) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -55,7 +53,7 @@ public class Fornecedor implements Serializable {
 
     @Override
     public String toString() {
-        return "br.com.sw2.comercial.bean.Fornecedor[ cnpj=" + cnpj + " ]";
+        return "br.com.sw2.comercial.bean.Produto[ id=" + id + " ]";
     }
     
 }
--- fontes/Comercial/src/br/com/sw2/comercial/bean/TipoProduto.java
+++ fontes/Comercial/src/br/com/sw2/comercial/bean/TipoProduto.java
@@ -8,46 +8,46 @@ package br.com.sw2.comercial.bean;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 /**
  *
  * @author marcelo
  */
 @Entity
-@Table(name="TblFornecedor")
-public class Fornecedor implements Serializable {
+public class TipoProduto implements Serializable {
     private static final long serialVersionUID = 1L;
-    
     @Id
-    private String cnpj;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    private String razaoSocial;
-    private String endereco;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String cep;
-    private String email;
-    private String fone1;
-    private String fone2;
+    private String nomeTipo;
     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (cnpj != null ? cnpj.hashCode() : 0);
+        hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Fornecedor)) {
+        if (!(object instanceof TipoProduto)) {
             return false;
         }
-        Fornecedor other = (Fornecedor) object;
-        if ((this.cnpj == null && other.cnpj != null) || (this.cnpj != null && !this.cnpj.equals(other.cnpj))) {
+        TipoProduto other = (TipoProduto) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -55,7 +55,7 @@ public class Fornecedor implements Serializable {
 
     @Override
     public String toString() {
-        return "br.com.sw2.comercial.bean.Fornecedor[ cnpj=" + cnpj + " ]";
+        return "br.com.sw2.comercial.bean.TipoProduto[ id=" + id + " ]";
     }
     
 }
