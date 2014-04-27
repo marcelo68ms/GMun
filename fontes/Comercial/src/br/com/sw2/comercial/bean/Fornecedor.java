89a3f611173841a20c1da32834a57948258d948a
Marcelo Santos <marcelo68ms@gmail.com>
2014-04-27 18:36:01 -0300

    Desenvolvimento do Código

--- fontes/Comercial/src/br/com/sw2/comercial/bean/Fornecedor.java
+++ fontes/Comercial/src/br/com/sw2/comercial/bean/Fornecedor.java
@@ -11,12 +11,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author marcelo
  */
 @Entity
+@Table(name="TblFornecedor")
 public class Fornecedor implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
