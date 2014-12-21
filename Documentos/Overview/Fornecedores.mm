bf098e833cc7b389620929768221f871907fd9de
Marcelo Santos <marcelo68ms@gmail.com>
2014-12-21 16:06:52 -0200

    Acerto do Mapa Mental.

--- Documentos/Overview/Fornecedores.mm
+++ Documentos/Overview/Fornecedores.mm
@@ -1,4 +1,17 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1419175551714" ID="ID_696568720" MODIFIED="1419175561509" TEXT="Fornecedores"/>
+<node CREATED="1419175551714" ID="ID_696568720" MODIFIED="1419176017654" TEXT="Fornecedores">
+<node CREATED="1419175801766" ID="ID_1129712147" MODIFIED="1419175808676" POSITION="right" TEXT="Tipos de Produto">
+<node CREATED="1419175813406" ID="ID_1579804329" MODIFIED="1419175817197" TEXT="Produtos">
+<node CREATED="1419176488034" ID="ID_980402982" MODIFIED="1419176496750" TEXT="Descri&#xe7;ao detalhada"/>
+<node CREATED="1419176497717" ID="ID_1740776914" MODIFIED="1419176504498" TEXT="Fotos do Produto"/>
+</node>
+</node>
+<node CREATED="1419176034209" ID="ID_330257777" MODIFIED="1419176095381" POSITION="left" TEXT="Rela&#xe7;&#xe3;o de Produtos com Procedimentos"/>
+<node CREATED="1419176441300" ID="ID_72948521" MODIFIED="1419176449181" POSITION="right" TEXT="Lista de Pre&#xe7;os dos Produtos">
+<node CREATED="1419176457991" ID="ID_1381183426" MODIFIED="1419176462793" TEXT="Validade dos Pre&#xe7;os"/>
+<node CREATED="1419176463650" ID="ID_142082039" MODIFIED="1419176470283" TEXT="Condi&#xe7;&#xf5;es de Pagamentos"/>
+<node CREATED="1419176470830" ID="ID_1631104947" MODIFIED="1419176476668" TEXT="Condi&#xe7;&#xf5;es de Entrega"/>
+</node>
+</node>
 </map>
