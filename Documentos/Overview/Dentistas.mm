30ee64fe14adb8a15298d48dd111fa798fe37eb3
Marcelo Santos <marcelo68ms@gmail.com>
2014-12-21 11:39:27 -0200

    Criação dos Mapas Mentais do Produto.

---
+++ Documentos/Overview/Dentistas.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1419168421463" ID="ID_272671514" MODIFIED="1419168441248" TEXT="Dentistas">
+<node CREATED="1419168666178" ID="ID_530289207" MODIFIED="1419168676730" POSITION="right" TEXT="Cadastro de Procedimentos">
+<node CREATED="1419168681388" ID="ID_1600397675" MODIFIED="1419168693060" TEXT="Pre&#xe7;os por Procedimento"/>
+</node>
+<node CREATED="1419168950259" ID="ID_669061150" MODIFIED="1419168963478" POSITION="left" TEXT="Cadastro de Conv&#xea;nios"/>
+</node>
+</map>
