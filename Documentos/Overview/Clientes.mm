e1ff89a1ea1127c104661f291dec5d38a8fc91bb
marcelo.santos <marcelo68ms@gmail.com>
2014-12-23 09:06:31 -0200

    Trabalho com os Mapas Mentais.

---
+++ Documentos/Overview/Clientes.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ccccff" CREATED="1419327741893" ID="ID_1775484795" MODIFIED="1419332728740" TEXT="Clientes">
+<node CREATED="1419327760333" ID="ID_1463324792" MODIFIED="1419327762534" POSITION="right" TEXT="Agenda">
+<node CREATED="1419327769593" ID="ID_1692285439" MODIFIED="1419327776803" TEXT="Consultas Marcadas">
+<node CREATED="1419327778619" ID="ID_1903484118" MODIFIED="1419327993441" TEXT="Dia, Hor&#xe1;rio e Dentista"/>
+<node CREATED="1419327955856" ID="ID_80434763" MODIFIED="1419327988698" TEXT="Procedimentos por Seq&#xfc;&#xea;ncia"/>
+</node>
+</node>
+<node CREATED="1419329811317" ID="ID_125300592" MODIFIED="1419329832723" POSITION="left" TEXT="Conta Corrente do Tratamento"/>
+</node>
+</map>
