0532119451aea67368b30a7cc8f307df66e6eb88
marcelo.santos <marcelo68ms@gmail.com>
2014-12-24 11:30:18 -0200

    Inicio do Trabalho com a Prototipação.

--- Documentos/Overview/Clientes.mm
+++ Documentos/Overview/Clientes.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node BACKGROUND_COLOR="#ccccff" CREATED="1419327741893" ID="ID_1775484795" MODIFIED="1419360374267" TEXT="Clientes">
+<node BACKGROUND_COLOR="#cc00cc" CREATED="1419327741893" ID="ID_1775484795" MODIFIED="1419427649510" TEXT="Clientes">
 <node CREATED="1419327760333" ID="ID_1463324792" MODIFIED="1419327762534" POSITION="right" TEXT="Agenda">
 <node CREATED="1419327769593" ID="ID_1692285439" MODIFIED="1419327776803" TEXT="Consultas Marcadas">
 <node CREATED="1419327778619" ID="ID_1903484118" MODIFIED="1419327993441" TEXT="Dia, Hor&#xe1;rio e Dentista"/>
