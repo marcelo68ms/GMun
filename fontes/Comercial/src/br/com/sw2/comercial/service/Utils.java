68459f0bed15bbb2df61d0d18e3acba880535271
Djane <djaneluz@gmail.com>
2015-01-15 15:52:19 -0200

    Refactoring

--- fontes/Comercial/src/br/com/sw2/comercial/service/Utils.java
+++ fontes/Comercial/src/br/com/sw2/comercial/service/Utils.java
@@ -1,13 +1,19 @@
 package br.com.sw2.comercial.service;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Utils {
 	
-	public static String readURL(URL url) throws IOException {
+	public static String readURL(String stringUrl) throws IOException {
+		URL url = new URL(stringUrl);
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		InputStream is = url.openStream();
 		int r;
@@ -18,4 +24,40 @@ public class Utils {
 	}
 
 
+	public static String sendPost(String url, String urlParameters) throws IOException {
+
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		// add request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		System.out.println(response);
+
+		return response.toString();
+
+	}
+	
 }
