8114cddd4e11e1c64699ace18ae4de133391241d
Djane <djaneluz@gmail.com>
2015-01-23 18:22:14 -0200

    Correção

--- fontes/Comercial/src/br/com/sw2/comercial/service/paypal/ExecutePaymentServlet.java
+++ fontes/Comercial/src/br/com/sw2/comercial/service/paypal/ExecutePaymentServlet.java
@@ -54,7 +54,7 @@ public class ExecutePaymentServlet extends HttpServlet {
 		post.addHeader(Utils.AUTHORIZATION, Utils.TOKEN_TYPE + SimplePaymentService.getToken());
 		post.addHeader(Utils.CONTENT_TYPE, Utils.APPLICATION_JSON);
 
-		StringBuffer urlParameters = new StringBuffer("{\"").append(Utils.PAYER_ID).append("\":\"").append(payerId).append("\"}");
+		StringBuffer urlParameters = new StringBuffer("{\"payer_id\":\"").append(payerId).append("\"}");
 		
 		try {
 			post.setEntity(new StringEntity(urlParameters.toString()));
